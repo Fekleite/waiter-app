0236b73a5dfad01f79c95ac1ca65cecbfc3a83da
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-07-15 23:37:50 -0300

    feat: create product details modal

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,17 +1,44 @@
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 
 import { products } from '../mocks/products';
+import type { Product } from '../types/product';
 
+import { ProductDetailsModal } from './product-details-modal';
 import { ProductItem } from './product-item';
 import { Separator } from './separator';
 
 export function Menu() {
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [isProductDetailsModalVisible, setProductDetailsModalVisible] =
+    useState(false);
+
+  function handleSeeProductDetails(product: Product) {
+    setSelectedProduct(product);
+    setProductDetailsModalVisible(true);
+  }
+
+  function handleCloseProductDetailModal() {
+    setProductDetailsModalVisible(false);
+  }
+
   return (
-    <FlatList
-      data={products}
-      keyExtractor={(product) => product._id}
-      renderItem={({ item }) => <ProductItem product={item} />}
-      ItemSeparatorComponent={Separator}
-    />
+    <>
+      <FlatList
+        data={products}
+        keyExtractor={(product) => product._id}
+        renderItem={({ item }) => (
+          <ProductItem product={item} onSeeDetails={handleSeeProductDetails} />
+        )}
+        ItemSeparatorComponent={Separator}
+      />
+
+      <ProductDetailsModal
+        visible={isProductDetailsModalVisible}
+        onRequestClose={handleCloseProductDetailModal}
+        onClose={handleCloseProductDetailModal}
+        product={selectedProduct}
+      />
+    </>
   );
 }
---
+++ src/components/product-details-modal.tsx
@@ -0,0 +1,152 @@
+import {
+  FlatList,
+  ImageBackground,
+  Modal,
+  type ModalProps,
+  Pressable,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import { Close } from '../assets/icons/close';
+
+import { defaultImageHost } from '../env';
+import type { Product } from '../types/product';
+import { formatToCurrency } from '../utils/number-format';
+
+import { Button } from './button';
+import { Container } from './container';
+import { Text } from './text';
+import { TextCard } from './text-card';
+
+interface ProductDetailsModalProps extends ModalProps {
+  product: Product | null;
+  onClose: () => void;
+}
+
+export function ProductDetailsModal({
+  product,
+  onClose,
+  ...props
+}: ProductDetailsModalProps) {
+  if (!product) {
+    return null;
+  }
+
+  return (
+    <Modal {...props} animationType="slide" presentationStyle="pageSheet">
+      <View style={styles.modalContainer}>
+        <ImageBackground
+          source={{
+            uri: `${defaultImageHost}/${product.imagePath}`,
+          }}
+          style={styles.image}
+        >
+          <Pressable
+            style={({ pressed }) => [
+              styles.closeButton,
+              pressed ? { opacity: 0.5 } : {},
+            ]}
+            onPress={onClose}
+          >
+            <Close color="#ffffff" />
+          </Pressable>
+        </ImageBackground>
+
+        <View style={styles.modalContent}>
+          <View style={styles.title}>
+            <Text weight="600" size={18}>
+              {product.name}
+            </Text>
+            <Text color="#666666">{product.description}</Text>
+          </View>
+
+          {product.ingredients.length > 0 && (
+            <View style={styles.ingredientsList}>
+              <Text weight="600" color="#666666">
+                Ingredientes
+              </Text>
+
+              <FlatList
+                data={product.ingredients}
+                keyExtractor={(ingredient) => ingredient._id}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => (
+                  <TextCard
+                    label={item.name}
+                    icon={item.icon}
+                    style={{ marginBottom: 4 }}
+                  />
+                )}
+              />
+            </View>
+          )}
+        </View>
+
+        <Container style={styles.footerContainer}>
+          <View style={styles.modalFooter}>
+            <View>
+              <Text color="#666666">Preço</Text>
+
+              <Text weight="600" size={18}>
+                {formatToCurrency(product.price)}
+              </Text>
+            </View>
+
+            <Button>Adicionar ao pedido</Button>
+          </View>
+        </Container>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+  },
+  closeButton: {
+    borderRadius: 50,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    width: 32,
+    height: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 24,
+    right: 24,
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  modalContent: {
+    flex: 1,
+    paddingTop: 32,
+    paddingHorizontal: 24,
+    gap: 32,
+  },
+  title: {
+    gap: 16,
+  },
+  ingredientsList: {
+    flex: 1,
+    gap: 16,
+  },
+  input: {
+    borderRadius: 8,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+  },
+  footerContainer: {
+    backgroundColor: '#ffffff',
+  },
+  modalFooter: {
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});

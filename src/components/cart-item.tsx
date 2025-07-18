957849489043873ccc05a15abab4e77ccdae3976
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-07-17 22:34:24 -0300

    feat: create cart item component

---
+++ src/components/cart-item.tsx
@@ -0,0 +1,94 @@
+import { Image, Pressable, StyleSheet, View } from 'react-native';
+
+import { MinusCircle } from '../assets/icons/minus-circle';
+import { PlusCircle } from '../assets/icons/plus-circle';
+
+import { defaultImageHost } from '../env';
+import type { Item } from '../types/cart';
+import { formatToCurrency } from '../utils/number-format';
+
+import { Text } from './text';
+
+interface CartItemProps {
+  item: Item;
+}
+
+export function CartItem({ item }: CartItemProps) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.product}>
+        <Image
+          source={{
+            uri: `${defaultImageHost}/${item.product.imagePath}`,
+            width: 48,
+            height: 40,
+          }}
+          style={styles.image}
+        />
+
+        <Text size={14} color="#999999">
+          {item.quantity}x
+        </Text>
+
+        <View style={styles.details}>
+          <Text weight="600" size={14}>
+            {item.product.name}
+          </Text>
+          <Text size={14} color="#666666">
+            {formatToCurrency(item.product.price)}
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.actions}>
+        <Pressable
+          style={({ pressed }) => [
+            styles.actionButton,
+            { opacity: pressed ? 0.5 : 1 },
+          ]}
+        >
+          <PlusCircle />
+        </Pressable>
+
+        <Pressable
+          style={({ pressed }) => [
+            styles.actionButton,
+            { opacity: pressed ? 0.5 : 1 },
+          ]}
+        >
+          <MinusCircle />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 16,
+    paddingVertical: 8,
+  },
+  product: {
+    flexDirection: 'row',
+    gap: 12,
+  },
+  image: {
+    width: 48,
+    height: 40,
+    borderRadius: 6,
+  },
+  details: {
+    gap: 4,
+  },
+  actions: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+  },
+  actionButton: {
+    padding: 4,
+  },
+});
---
+++ src/types/cart.ts
@@ -0,0 +1,6 @@
+import type { Product } from './product';
+
+export interface Item {
+  product: Product;
+  quantity: number;
+}

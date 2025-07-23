bbca467468b211c92c7c58a57ab093e4791e99b9
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-07-22 23:04:49 -0300

    feat: add loading state on button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,17 @@
-import { Pressable, type PressableProps, StyleSheet } from 'react-native';
+import {
+  ActivityIndicator,
+  Pressable,
+  type PressableProps,
+  StyleSheet,
+} from 'react-native';
 import { Text } from './text';
 
 interface ButtonProps extends PressableProps {
   children: string;
+  loading?: boolean;
 }
 
-export function Button({ children, disabled, ...props }: ButtonProps) {
+export function Button({ children, disabled, loading, ...props }: ButtonProps) {
   return (
     <Pressable
       style={({ pressed }) => [
@@ -13,11 +19,16 @@ export function Button({ children, disabled, ...props }: ButtonProps) {
         { opacity: pressed ? 0.8 : 1 },
         { backgroundColor: disabled ? '#cccccc' : '#d73035' },
       ]}
+      disabled={disabled || loading}
       {...props}
     >
-      <Text color="#ffffff" weight="600">
-        {children}
-      </Text>
+      {loading ? (
+        <ActivityIndicator color="#ffffff" />
+      ) : (
+        <Text color="#ffffff" weight="600">
+          {children}
+        </Text>
+      )}
     </Pressable>
   );
 }
